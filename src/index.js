fe2a39eb3e858ebf83d75fa6a65fb50d439f4458
savitha-gollamudi <savitha.gollamudi@gmail.com>
2019-12-28 21:53:01 +0530

    Add SASS loader to override bootstrap colors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // wntry point to webpack
 import {run} from "./app/app"
-import "./main.css"
+import "./main.scss"
 import {AlertService} from "./app/alert.service"
 import {ComponentService} from "./app/component.service"
 const alertService = new AlertService();

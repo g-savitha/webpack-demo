337ee79367f4839e8a7c4198db16a7e394c133ce
savitha-gollamudi <savitha.gollamudi@gmail.com>
2019-12-28 21:43:20 +0530

    Add loaders to handle css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // wntry point to webpack
 import {run} from "./app/app"
+import "./main.css"
 import {AlertService} from "./app/alert.service"
 import {ComponentService} from "./app/component.service"
 const alertService = new AlertService();
